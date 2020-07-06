d760107ebb21742a7ae9291568b8e025157e8040
tientp_sf <Tran.Phuoc.Tien@steadfasttech.com.au>
2020-07-07 06:18:44 +0700

    Init

---
+++ Dashboard/Dashboard/Controllers/OfferController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dashboard.Controllers
+{
+    public class OfferController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Detail()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
